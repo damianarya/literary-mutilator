e519f641f02cef26d7d43e29cad8a2db43aa79e8
Damian Arya <dahakcyrus@yahoo.com>
2019-11-10 15:56:55 -0700

    added functions

--- literary-mutilator.js
+++ literary-mutilator.js
@@ -1,4 +1,37 @@
-gohe-el, zodacare eca ca-no-quoda! Hecate Hecate Lilith Ol sonuf vaoresaji gohu IAD Balata Vi-i-vau el! Sobame ial-pereji i-zoda-zodazod pi-adapehe casarema aberameji ta ta-labo paracaleda qo-ta lores-el-qo turebesa oogebalatohe Norezodacahisa otahila Gigipahe	gohe-el zodacare eca ca-no-quoda! Hecate Hecate Lilith Ol sonuf vaoresaji gohu IAD Balata Vi-i-vau el! Sobame ial-pereji i-zoda-zodazod pi-adapehe casarema aberameji ta ta-labo paracaleda qo-ta lores-el-qo turebesa oogebalatohe! Norezodacahisa otahila Gigipahe.	gohe-el, zodacare eca ca-no-quoda! Hecate Hecate Lilith Ol sonuf vaoresaji gohu IAD Balata Vi-i-vau el! Sobame ial-pereji i-zoda-zodazod pi-adapehe casarema aberameji ta ta-labo paracaleda qo-ta lores-el-qo turebesa oogebalatohe Norezodacahisa otahila Gigipahe	gohe-el zodacare eca ca-no-quoda! Hecate Hecate Lilith Ol sonuf vaoresaji gohu IAD Balata Vi-i-vau el! Sobame ial-pereji i-zoda-zodazod pi-adapehe casarema aberameji ta ta-labo paracaleda qo-ta lores-el-qo turebesa oogebalatohe! Norezodacahisa otahila Gigipahe.
+window.onkeydown = function () {
+	highlight ();
+
+};
+//.operators for javaScript
+function highlight() {
+	document.getElementById("p1").style.backgroundColor = "#33333"
+}
+//oreder of functions does not matter as long as called correctly in html
+//must link to html file
+function changeFontFamily () {
+	document.getElementById("p1").style.fontFamily = `monospace`;
+}
+
+function findReplace() {
+	let txt = document.getElementById("find-replace").innerHTML;
+							}
+	let findword = document.getElementById("texFind").value;
+
+	let findword = document.getElementById("texFind").value;
+
+	//create new regex object. include the g and i modifyers for global and case insensitive
+let re = new  regularExpresion(findword, "gi");
+
+//find and replace words in original txt using created regex
+
+let  newText = txt.replace(re, replaceWord);
+//output new txt
+document.getElementById("find-replace").innerHTML = newText;
+gohe-el, zodacare eca ca-no-quoda! Hecate Hecate Lilith Ol sonuf vaoresaji gohu IAD Balata Vi-i-vau el! Sobame ial-pereji i-zoda-zodazod pi-adapehe casarema aberameji ta ta-labo paracaleda qo-ta lores-el-qo turebesa oogebalatohe Norezodacahisa otahila Gigipahe	gohe-el zodacare eca ca-no-quoda! Hecate Hecate Lilith Ol sonuf vaoresaji gohu IAD Balata Vi-i-vau el! Sobame ial-pereji i-zoda-zodazod pi-adapehe casarema aberameji ta ta-labo
+
+
+
+paracaleda qo-ta lores-el-qo turebesa oogebalatohe! Norezodacahisa otahila Gigipahe.	gohe-el, zodacare eca ca-no-quoda! Hecate Hecate Lilith Ol sonuf vaoresaji gohu IAD Balata Vi-i-vau el! Sobame ial-pereji i-zoda-zodazod pi-adapehe casarema aberameji ta ta-labo paracaleda qo-ta lores-el-qo turebesa oogebalatohe Norezodacahisa otahila Gigipahe	gohe-el zodacare eca ca-no-quoda! Hecate Hecate Lilith Ol sonuf vaoresaji gohu IAD Balata Vi-i-vau el! Sobame ial-pereji i-zoda-zodazod pi-adapehe casarema aberameji ta ta-labo paracaleda qo-ta lores-el-qo turebesa oogebalatohe! Norezodacahisa otahila Gigipahe.
 
 	Damballa Behemoth Naamah Balaam Ili e-Ol balazodareji od aala tahilanu-os netaabe: daluga vaomesareji elonusa cape-mi-ali varoesa cala homila; Ahpuch Kali Thoth Loki Abaddon qui-i-inu toltoregi cahisa i cahisaji em ozodien Yen-lo-Wang Damballa Do-o-i-ape mada goholore gohus, amiranu! Micama! Yehusozod ca-ca-com, od do-o-a-inu noari micaolazoda a-ai-om.	Damballa Behemoth Naamah Balaam Ili e-Ol balazodareji, od aala tahilanu-os neta abe daluga vaomesareji elonusa cape-mi-ali varoesa cala homila Ahpuch Kali Thoth Loki Abaddon qui-i-inu toltoregi cahisa i cahisaji em ozodien; Yen-lo-Wang Damballa Do-o-i-ape mada: goholore, gohus, amiranu! Micama! Yehusozod ca-ca-com, od do-o-a-inu noari micaolazoda a-ai-om.
 
