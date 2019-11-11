5007ed19f7967c051c1c6d84201a9bbaec5d7392
Damian Arya <dahakcyrus@yahoo.com>
2019-11-11 16:54:44 -0700

    added a few functions to no avail

--- literary-mutilator.js
+++ literary-mutilator.js
@@ -6,14 +6,14 @@ window.onkeydown = function () {
 function highlight() {
 	document.getElementById("p1").style.backgroundColor = "#33333"
 }
-//oreder of functions does not matter as long as called correctly in html
+//order of functions does not matter as long as called correctly in html
 //must link to html file
 function changeFontFamily () {
 	document.getElementById("p1").style.fontFamily = `monospace`;
 }
 
 function findReplace() {
-	let txt = document.getElementById("find-replace").innerHTML;
+	let txt = document.getElementById("find text").innerHTML;
 							}
 	let findword = document.getElementById("texFind").value;
 
@@ -27,12 +27,20 @@ let re = new  regularExpresion(findword, "gi");
 let  newText = txt.replace(re, replaceWord);
 //output new txt
 document.getElementById("find-replace").innerHTML = newText;
-gohe-el, zodacare eca ca-no-quoda! Hecate Hecate Lilith Ol sonuf vaoresaji gohu IAD Balata Vi-i-vau el! Sobame ial-pereji i-zoda-zodazod pi-adapehe casarema aberameji ta ta-labo paracaleda qo-ta lores-el-qo turebesa oogebalatohe Norezodacahisa otahila Gigipahe	gohe-el zodacare eca ca-no-quoda! Hecate Hecate Lilith Ol sonuf vaoresaji gohu IAD Balata Vi-i-vau el! Sobame ial-pereji i-zoda-zodazod pi-adapehe casarema aberameji ta ta-labo
 
+addEventListener style.event.target.color = blue;
 
+// this is supposed to find and re[place text
+document.addEventListener("DomContentLoaded", function() {
+	let btn = getElementById("change text");
+	let btn.addEventListener("click", findReplace());
+});
 
-paracaleda qo-ta lores-el-qo turebesa oogebalatohe! Norezodacahisa otahila Gigipahe.	gohe-el, zodacare eca ca-no-quoda! Hecate Hecate Lilith Ol sonuf vaoresaji gohu IAD Balata Vi-i-vau el! Sobame ial-pereji i-zoda-zodazod pi-adapehe casarema aberameji ta ta-labo paracaleda qo-ta lores-el-qo turebesa oogebalatohe Norezodacahisa otahila Gigipahe	gohe-el zodacare eca ca-no-quoda! Hecate Hecate Lilith Ol sonuf vaoresaji gohu IAD Balata Vi-i-vau el! Sobame ial-pereji i-zoda-zodazod pi-adapehe casarema aberameji ta ta-labo paracaleda qo-ta lores-el-qo turebesa oogebalatohe! Norezodacahisa otahila Gigipahe.
-
-	Damballa Behemoth Naamah Balaam Ili e-Ol balazodareji od aala tahilanu-os netaabe: daluga vaomesareji elonusa cape-mi-ali varoesa cala homila; Ahpuch Kali Thoth Loki Abaddon qui-i-inu toltoregi cahisa i cahisaji em ozodien Yen-lo-Wang Damballa Do-o-i-ape mada goholore gohus, amiranu! Micama! Yehusozod ca-ca-com, od do-o-a-inu noari micaolazoda a-ai-om.	Damballa Behemoth Naamah Balaam Ili e-Ol balazodareji, od aala tahilanu-os neta abe daluga vaomesareji elonusa cape-mi-ali varoesa cala homila Ahpuch Kali Thoth Loki Abaddon qui-i-inu toltoregi cahisa i cahisaji em ozodien; Yen-lo-Wang Damballa Do-o-i-ape mada: goholore, gohus, amiranu! Micama! Yehusozod ca-ca-com, od do-o-a-inu noari micaolazoda a-ai-om.
-
-	Cimeries Pilahe farezodem zodenurezoda adana gono Ia dapiel das home – tohe: soba ipame luipamis Loki Metztli Nergal Typhon Abaddon vaunid-el-cahisa ta-pu-ime qo-mos-pelehe telocahe; qui-i-inu toltoregi cahisa icahisaji em ozodien qui-i-inu toltoregi cahisa i cahisaji em ozodienCimeries Pilahe farezodem zodenurezoda adana gono Ia dapiel das home – tohe: soba ipame luipamis Loki Metztli Nergal Typhon Abaddon vaunid-el-cahisa ta-pu-ime qo-mos-pelehe telocahe; qui-i-inu toltoregi cahisa icahisaji em ozodien qui-i-inu toltoregi cahisa i cahisaji em ozodien.
+function findandReplace() {
+	let string = document.getElementById("input").innerHTML;
+	let findWord = document.getElementById("find").value;
+	let replaceValue = document.getElementById("replace").value;
+	let reg = new regExp(findWord, "gi");
+	let newText = string.replace(reg, replaceValue);
+document.getElementById("input").innerHTML = newText;
+}
